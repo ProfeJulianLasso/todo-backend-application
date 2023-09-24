c3cad11b6431c3e4f00d0e9fa37e3b14fc377109
Julian Lasso <julian.lasso@sofka.com.co>
2023-09-23 19:38:39 -0500

    feat: :sparkles: implementación del archivo app.ts como entrada al sistema

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,90 @@
-export class HelloWorld {
-  public sayHello(message: string) {
-    console.log('Hello World! ' + message);
+import { SecurityAggregate, ToDoAggregate } from '@domain';
+import {
+  CompleteToDoCommandInput,
+  CreateToDoCommandInput,
+  CreateUserCommandInput,
+  GetAllToDosCommandInput,
+  LoginCommandInput,
+} from './commands';
+import { AppConfig } from './common';
+import {
+  ToDoModel,
+  ToDosBaseRepository,
+  UserModel,
+  UsersBaseRepository,
+} from './persistence';
+import {
+  CompleteToDoUseCase,
+  CreateToDoUseCase,
+  CreateUserUseCase,
+  GetAllToDosUseCase,
+  LoginUseCase,
+} from './use-cases';
+
+export class App {
+  private static _instance: App | null = null;
+  private readonly securityAggregate: SecurityAggregate;
+  private readonly toDoAggregate: ToDoAggregate;
+
+  private constructor() {
+    this.securityAggregate = new SecurityAggregate();
+    this.toDoAggregate = new ToDoAggregate();
+  }
+
+  static get instance(): App {
+    if (!App._instance) App._instance = new App();
+    return App._instance;
+  }
+
+  login(
+    loginCommand: LoginCommandInput,
+    dependence: { userRepository: UsersBaseRepository }
+  ): Promise<string> {
+    const useCase = new LoginUseCase(
+      AppConfig.instance,
+      dependence.userRepository
+    );
+    return useCase.execute(loginCommand);
+  }
+
+  createUser(
+    createUserCommand: CreateUserCommandInput,
+    dependence: { userRepository: UsersBaseRepository }
+  ): Promise<UserModel> {
+    const useCase = new CreateUserUseCase(
+      this.securityAggregate,
+      dependence.userRepository
+    );
+    return useCase.execute(createUserCommand);
+  }
+
+  createToDo(
+    createToDoCommand: CreateToDoCommandInput,
+    dependence: { toDoRepository: ToDosBaseRepository }
+  ): Promise<ToDoModel> {
+    const useCase = new CreateToDoUseCase(
+      this.toDoAggregate,
+      dependence.toDoRepository
+    );
+    return useCase.execute(createToDoCommand);
+  }
+
+  completeToDo(
+    completeToDoCommand: CompleteToDoCommandInput,
+    dependence: { toDoRepository: ToDosBaseRepository }
+  ): Promise<ToDoModel> {
+    const useCase = new CompleteToDoUseCase(
+      this.toDoAggregate,
+      dependence.toDoRepository
+    );
+    return useCase.execute(completeToDoCommand);
+  }
+
+  getAllToDos(
+    getAllToDosCommand: GetAllToDosCommandInput,
+    dependence: { toDoRepository: ToDosBaseRepository }
+  ): Promise<ToDoModel[]> {
+    const useCase = new GetAllToDosUseCase(dependence.toDoRepository);
+    return useCase.execute(getAllToDosCommand);
   }
 }
-
-const hello = new HelloWorld();
-hello.sayHello('domain');
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
 export * from './app';
+export * from './commands';
+export * from './common';
+export * from './persistence';
+export * from './use-cases';
