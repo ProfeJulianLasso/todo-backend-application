ff5ee972aeb1fc9d99c772cd622154bd632ffb9f
Julian Lasso <julian.lasso@sofka.com.co>
2023-09-23 20:14:19 -0500

    test: :construction: estructura inicial para las pruebas unitarias
    algunas pruebas fueron migradas desde la capa de dominio

---
+++ tests/commands/validators/login.validator.spec.ts
@@ -0,0 +1,45 @@
+import { UserEmailValueObject, UserPasswordValueObject } from '@domain';
+import {
+  ApplicationException,
+  LoginCommand,
+  LoginValidator,
+} from '../../../src';
+
+describe('LoginValidator', () => {
+  describe('when the scenario has a valid data', () => {
+    it('should to be valid', () => {
+      // Arrange
+      const data = {
+        email: new UserEmailValueObject('test@domain.com'),
+        password: new UserPasswordValueObject('Password123!'),
+      } as LoginCommand;
+      const expected = true;
+
+      // Act
+      const validator = new LoginValidator(data);
+      validator.validate();
+      const result = validator.isValid();
+
+      // Assert
+      expect(result).toBe(expected);
+    });
+  });
+
+  describe('when the scenario has an invalid data', () => {
+    it('should to be invalid with invalid email', () => {
+      // Arrange
+      const data = {
+        email: new UserEmailValueObject('invalid-email'),
+        password: new UserPasswordValueObject('Password123!'),
+      } as LoginCommand;
+
+      // Act
+      const validator = new LoginValidator(data);
+      const exception = () => validator.validate();
+
+      // Assert
+      expect(exception).toThrow(ApplicationException);
+      expect(exception).toThrow('Invalid data');
+    });
+  });
+});
